ba6640dfccd8e125178cf5d0eac1695e31880779
xuna <2631962724@qq.com>
2017-07-11 22:44:51 +0800

    python调用百度API实现自动检测翻译

--- 项目五/Python_translate.py
+++ 项目五/Python_translate.py
@@ -5,11 +5,11 @@ import urllib,hashlib
 import random
 import requests,sys
 
-def getTransText():
-    
-	q = 'apple'
+
+def getTransText(in_text):
+	q = in_text
 	fromLang = 'auto'  #翻译源语言=自动检测
-	toLang = 'auto'    #译文语言 = 自动检测
+	toLang1 = 'auto'    #译文语言 = 自动检测
 
 	appid = '20170711000064108'
 	salt = random.randint(32768, 65536)
@@ -23,7 +23,7 @@ def getTransText():
      
     #计算完整请求
 	myurl = '/api/trans/vip/translate'
-	myurl = myurl+'?appid='+appid+'&q='+q+'&from='+fromLang+'&to='+toLang+'&salt='+str(salt)+'&sign='+sign
+	myurl = myurl+'?appid='+appid+'&q='+q+'&from='+fromLang+'&to='+toLang1+'&salt='+str(salt)+'&sign='+sign
 	url = "http://api.fanyi.baidu.com"+myurl
 
     # 发送请求
@@ -33,4 +33,6 @@ def getTransText():
 	#转换为字典类型
 	res = eval(res.text)
 	return (res["trans_result"][0]['dst'])
-print (getTransText())
+while(True):
+	in_text=input()
+	print (in_text+'  =  '+getTransText(in_text))
